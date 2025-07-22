dc4da6f1ae50bc59a22fbcdf5cbb9990c5160760
Javier Gonzalez <javierignaciogc@gmail.com>
2025-07-22 08:28:46 -0400

    Equipment component created and added to Player Controller.

---
+++ Plugins/SilentiumInventorySystem/Source/SilentiumInventorySystem/Private/EquipmentManagement/Components/SInv_EquipmentComponent.cpp
@@ -0,0 +1,59 @@
+﻿// Developed by Javier Gonzalez [Lumina Division]
+
+
+#include "EquipmentManagement/Components/SInv_EquipmentComponent.h"
+
+#include "GameFramework/Character.h"
+#include "InventoryManagement/Components/SInv_InventoryComponent.h"
+#include "InventoryManagement/Utils/SInv_InventoryStatics.h"
+
+
+void USInv_EquipmentComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OwningPlayerController = Cast<APlayerController>(GetOwner());
+
+	// Only if our owner is a Player controller we proceed.
+	if (OwningPlayerController.IsValid())
+	{
+		ACharacter* OwnerCharacter = Cast<ACharacter>(OwningPlayerController->GetPawn());
+		if (OwnerCharacter)
+		{
+			OwningSkeletalMesh = OwnerCharacter->GetMesh();
+		}
+		InitInventoryComponent();
+	}
+}
+
+void USInv_EquipmentComponent::InitInventoryComponent()
+{
+	InventoryComponent = USInv_InventoryStatics::GetInventoryComponent(OwningPlayerController.Get());
+	
+	if (!InventoryComponent.IsValid()) return;
+
+	/*------------------------------------------*/
+	/*			Equipment Delegates				*/
+	/*	Check if Delegates are already bound.   */
+	/*------------------------------------------*/
+	
+	if (!InventoryComponent->OnItemEquipped.IsAlreadyBound(this,&ThisClass::OnItemEquipped))
+	{
+		InventoryComponent->OnItemEquipped.AddDynamic(this,&ThisClass::OnItemEquipped);
+	}
+	if (!InventoryComponent->OnItemUnequipped.IsAlreadyBound(this,&ThisClass::OnItemUnequipped))
+	{
+		InventoryComponent->OnItemUnequipped.AddDynamic(this,&ThisClass::OnItemUnequipped);
+	}
+}
+
+void USInv_EquipmentComponent::OnItemEquipped(USInv_InventoryItem* EquippedItem)
+{
+	
+}
+
+void USInv_EquipmentComponent::OnItemUnequipped(USInv_InventoryItem* UnequippedItem)
+{
+	
+}
+
---
+++ Plugins/SilentiumInventorySystem/Source/SilentiumInventorySystem/Public/EquipmentManagement/Components/SInv_EquipmentComponent.h
@@ -0,0 +1,40 @@
+﻿// Developed by Javier Gonzalez [Lumina Division]
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SInv_EquipmentComponent.generated.h"
+
+class USInv_InventoryItem;
+class APlayerController;
+class USInv_InventoryComponent;
+class USkeletalMeshComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent),Blueprintable)
+class SILENTIUMINVENTORYSYSTEM_API USInv_EquipmentComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+private:
+
+	TWeakObjectPtr<USInv_InventoryComponent> InventoryComponent;
+	TWeakObjectPtr<APlayerController> OwningPlayerController;
+	TWeakObjectPtr<USkeletalMeshComponent> OwningSkeletalMesh;
+
+	/*------------------------------------------*/
+	/*			Equipment Delegates				*/
+	/*------------------------------------------*/
+	UFUNCTION()
+	void OnItemEquipped(USInv_InventoryItem* EquippedItem);
+	UFUNCTION()
+	void OnItemUnequipped(USInv_InventoryItem* UnequippedItem);
+	
+	void InitInventoryComponent();
+};
